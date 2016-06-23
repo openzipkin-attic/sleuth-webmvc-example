16f80b58f7ad1bef9001a88c074c4dc108ebeb53
Adrian Cole <acole@pivotal.io>
2016-06-23 14:18:57 +0800

    Initial import

---
+++ src/main/java/sleuth/webmvc/BackEnd.java
@@ -0,0 +1,22 @@
+package sleuth.webmvc;
+
+import java.util.Date;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BackEnd {
+
+  @RequestMapping("/api") String printDate() {
+    return new Date().toString();
+  }
+
+  /** The spring application name is used for the Zipkin service name */
+  public static void main(String[] args) {
+    SpringApplication.run(BackEnd.class,
+        "--spring.application.name=backend", "--server.port=9000");
+  }
+}
---
+++ src/main/java/sleuth/webmvc/FrontEnd.java
@@ -0,0 +1,39 @@
+package sleuth.webmvc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.Sampler;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@RestController
+public class FrontEnd {
+
+  @Autowired RestTemplate template;
+
+  @RequestMapping("/") String callBackEnd() {
+    return template.getForObject("http://localhost:9000/api", String.class);
+  }
+
+  /** Sleuth automatically adds trace interceptors when in the classpath */
+  @Bean
+  public RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
+
+  /** Sleuth won't start a trace unless a sampler bean is configured */
+  @Bean
+  public Sampler defaultSampler() {
+    return new AlwaysSampler();
+  }
+
+  /** The spring application name is used for the Zipkin service name */
+  public static void main(String[] args) {
+    SpringApplication.run(FrontEnd.class, "--spring.application.name=frontend");
+  }
+}
