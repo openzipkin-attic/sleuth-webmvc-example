423597f54287460b92d115558252a826c931f5bb
Adrian Cole <acole@pivotal.io>
2017-10-05 21:49:01 +0800

    Reports spans to xray

--- src/main/java/sleuth/webmvc/Backend.java
+++ src/main/java/sleuth/webmvc/Backend.java
@@ -3,13 +3,24 @@ package sleuth.webmvc;
 import java.util.Date;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.sleuth.zipkin2.ZipkinAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import zipkin2.Span;
+import zipkin2.reporter.Reporter;
+import zipkin2.reporter.xray_udp.XRayUDPReporter;
 
 @EnableAutoConfiguration
 @RestController
 public class Backend {
+
+  @Bean(ZipkinAutoConfiguration.REPORTER_BEAN_NAME)
+  Reporter<Span> spanReporter() {
+    return XRayUDPReporter.create();
+  }
 
   @RequestMapping("/api")
   public String printDate(@RequestHeader(name = "user_name", required = false) String username) {
--- src/main/java/sleuth/webmvc/Frontend.java
+++ src/main/java/sleuth/webmvc/Frontend.java
@@ -3,11 +3,16 @@ package sleuth.webmvc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.sleuth.zipkin2.ZipkinAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import zipkin2.Span;
+import zipkin2.reporter.Reporter;
+import zipkin2.reporter.xray_udp.XRayUDPReporter;
 
 @EnableAutoConfiguration
 @RestController
@@ -17,6 +22,11 @@ public class Frontend {
   @Autowired RestTemplate restTemplate;
 
   String backendBaseUrl = System.getProperty("spring.example.backendBaseUrl", "http://localhost:9000");
+
+  @Bean(ZipkinAutoConfiguration.REPORTER_BEAN_NAME)
+  Reporter<Span> spanReporter() {
+    return XRayUDPReporter.create();
+  }
 
   @RequestMapping("/") public String callBackend() {
     return restTemplate.getForObject(backendBaseUrl + "/api", String.class);
