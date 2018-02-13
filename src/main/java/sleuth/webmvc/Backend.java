840db866e493a193f5bf86a82c32ccb15e38eb20
Adrian Cole <acole@pivotal.io>
2018-02-13 11:14:32 +0800

    Shows how to use Wavefront
    Just set management.metrics.export.wavefront.api-token=XXXXX

--- src/main/java/sleuth/webmvc/Backend.java
+++ src/main/java/sleuth/webmvc/Backend.java
@@ -1,6 +1,9 @@
 package sleuth.webmvc;
 
+import brave.SpanCustomizer;
+import io.opentracing.Tracer;
 import java.util.Date;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAutoConfiguration
 @RestController
 public class Backend {
+  @Autowired Tracer tracer; // available by depending on 'io.opentracing.brave:brave-opentracing'
+  @Autowired SpanCustomizer customizer; // available by default
 
   @RequestMapping("/api") public String printDate() {
+    // the following are equivalent means to add a lookup tag to the current span
+    tracer.activeSpan().setTag("user-id", "abcd");
+    customizer.tag("user-name", "robby");
     return new Date().toString();
   }
 
