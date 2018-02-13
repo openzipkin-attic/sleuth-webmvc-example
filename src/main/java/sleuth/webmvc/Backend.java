10b73f4907ec0fbaba12200eb58c3291e75faf34
Adrian Cole <adriancole@users.noreply.github.com>
2018-02-13 10:27:59 +0800

    Shows how to trace MySQL without changing your code
    This changes the example to read the timestamp from MySQL instead of
    the Spring Boot Process. It adds a brave tracing interceptor to add
    details to the existing trace.
    
    https://github.com/openzipkin/brave/tree/master/instrumentation/mysql

--- src/main/java/sleuth/webmvc/Backend.java
+++ src/main/java/sleuth/webmvc/Backend.java
@@ -1,17 +1,19 @@
 package sleuth.webmvc;
 
-import java.util.Date;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @EnableAutoConfiguration
 @RestController
 public class Backend {
+  @Autowired JdbcTemplate jdbcTemplate;
 
   @RequestMapping("/api") public String printDate() {
-    return new Date().toString();
+    return jdbcTemplate.queryForObject("SELECT NOW()", String.class);
   }
 
   public static void main(String[] args) {
