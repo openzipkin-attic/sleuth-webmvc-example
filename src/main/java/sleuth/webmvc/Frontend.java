263ae9fea1f483a92588351b059c7e59d8ced2e5
Adrian Cole <adriancole@users.noreply.github.com>
2018-03-05 16:33:20 +0800

    Uses EnableAutoConfiguration not SpringBootApplication to avoid scanning (#10)
    To reduce problems in layering on this example, we remove the
    `SpringBootApplication` annotation in favor of `EnableAutoConfiguration`
    as `SpringBootApplication` scans and instantiates beans in the package.
    
    We don't want the frontend to scan and load beans from the backend's
    package basically.

--- src/main/java/sleuth/webmvc/Frontend.java
+++ src/main/java/sleuth/webmvc/Frontend.java
@@ -1,20 +1,23 @@
 package sleuth.webmvc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@EnableAutoConfiguration
 @RestController
 @CrossOrigin // So that javascript can be hosted elsewhere
 public class Frontend {
 
+  @Autowired RestTemplate restTemplate;
+
   @RequestMapping("/") public String callBackend() {
-    return restTemplate().getForObject("http://localhost:9000/api", String.class);
+    return restTemplate.getForObject("http://localhost:9000/api", String.class);
   }
 
   @Bean RestTemplate restTemplate() {
