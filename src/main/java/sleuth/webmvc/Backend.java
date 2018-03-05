4f1166d4605d9ecef6da76011dd59b6c317deae8
Adrian Cole <acole@pivotal.io>
2018-03-05 16:29:29 +0800

    Uses EnableAutoConfiguration not SpringBootApplication to avoid scanning
    To reduce problems in layering on this example, we remove the
    `SpringBootApplication` annotation in favor of `EnableAutoConfiguration`
    as `SpringBootApplication` scans and instantiates beans in the package.
    
    We don't want the frontend to scan and load beans from the backend's
    package basically.

--- src/main/java/sleuth/webmvc/Backend.java
+++ src/main/java/sleuth/webmvc/Backend.java
@@ -2,11 +2,11 @@ package sleuth.webmvc;
 
 import java.util.Date;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@EnableAutoConfiguration
 @RestController
 public class Backend {
 
