ee2facc348818f0b098a5150e33c47d6d9c36bbd
Zoltan Nagy <abesto0@gmail.com>
2018-08-02 13:49:47 +0200

    [circleci] Fix: bind mounts are not supported
    So instead we put all the runtime dependencies into the Docker build
    context, and so pass them into the ad-hoc runtime containers.

--- docker/run.sh
+++ docker/run.sh
@@ -3,4 +3,4 @@
 set -eou pipefail
 
 CLASS_NAME="$1"; shift
-./mvnw exec:java -Dexec.mainClass=sleuth.webmvc.$CLASS_NAME -Dlogging.level.org.springframework.cloud.sleuth=DEBUG -Dspring.zipkin.baseUrl=http://zipkin:9411 -Dspring.example.backendBaseUrl=http://backend:9000
+java -cp "target/dependency/*:target/*" -Dlogging.level.org.springframework.cloud.sleuth=DEBUG -Dspring.zipkin.baseUrl=http://zipkin:9411 -Dspring.example.backendBaseUrl=http://backend:9000 sleuth.webmvc.$CLASS_NAME
