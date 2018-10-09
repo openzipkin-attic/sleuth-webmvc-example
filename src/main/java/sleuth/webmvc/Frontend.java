a53a6ce9da3d328cedf0023e80300e559696df6f
Adrian Cole <acole@pivotal.io>
2018-10-09 11:41:13 +0800

    Shows how to trace Apache HttpClient requests
    This changes the example to use Apache `HttpClient` instead of
    `RestTemplate` to make the call from the frontend to the backend.
    
    https://github.com/openzipkin/brave/tree/master/instrumentation/httpclient

--- src/main/java/sleuth/webmvc/Frontend.java
+++ src/main/java/sleuth/webmvc/Frontend.java
@@ -1,5 +1,12 @@
 package sleuth.webmvc;
 
+import brave.http.HttpTracing;
+import brave.httpclient.TracingHttpClientBuilder;
+import java.io.IOException;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -7,23 +14,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @EnableAutoConfiguration
 @RestController
 @CrossOrigin // So that javascript can be hosted elsewhere
 public class Frontend {
 
-  @Autowired RestTemplate restTemplate;
+  @Autowired HttpClient httpClient;
 
   String backendBaseUrl = System.getProperty("spring.example.backendBaseUrl", "http://localhost:9000");
 
-  @RequestMapping("/") public String callBackend() {
-    return restTemplate.getForObject(backendBaseUrl + "/api", String.class);
+  @RequestMapping("/") public String callBackend() throws IOException {
+    HttpResponse response = httpClient.execute(new HttpGet(backendBaseUrl + "/api"));
+    return new BasicResponseHandler().handleResponse(response);
   }
 
-  @Bean RestTemplate restTemplate() {
-    return new RestTemplate();
+  /** Apache HC clients aren't traced by default. This creates a traced instance. */
+  @Bean HttpClient httpClient(HttpTracing httpTracing) {
+    return TracingHttpClientBuilder.create(httpTracing).build();
   }
 
   public static void main(String[] args) {
