29db8fc99a688d49bc22139c2e7071d82ffd75bc
Adrian Cole <adriancole@users.noreply.github.com>
2020-06-03 13:59:24 +0800

    Latest sleuth and propagate something by default (#38)

--- src/main/java/sleuth/webmvc/Backend.java
+++ src/main/java/sleuth/webmvc/Backend.java
@@ -3,6 +3,7 @@ package sleuth.webmvc;
 import java.util.Date;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Backend {
 
-  @RequestMapping("/api") public String printDate() {
+  @RequestMapping("/api")
+  public String printDate(@RequestHeader(name = "user_name", required = false) String username) {
+    if (username != null) {
+      return new Date().toString() + " " + username;
+    }
     return new Date().toString();
   }
 
